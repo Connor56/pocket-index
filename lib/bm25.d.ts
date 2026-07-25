802b295a03e009308c2ab1208a4610bbb88cd2ef
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2026-07-25 18:44:58 +0100

    feat(bm25): add types of the bm25 index

---
+++ lib/bm25.d.ts
@@ -0,0 +1,48 @@
+import type { Doc, SearchResult } from './models.d.ts'
+
+export interface BM25IndexOptions {
+  extractor: (text: string) => string[]
+  k1?: number
+  b?: number
+  fuzzyThreshold?: number
+}
+
+export interface BM25Doc {
+  keywordMap: Record<string, number>
+  length: number
+}
+
+export declare class BM25Index {
+  constructor(opts: BM25IndexOptions)
+  add(documents: Doc[]): Promise<void>
+  serialize(): Buffer
+  save(path: string): void
+  load(path: string | Buffer): void
+  search(query: string, topK?: number | null): Promise<SearchResult[]>
+  remove(id: string): void
+  list(): string[]
+  contains(id: string): boolean
+
+  _loadFromBinary(buffer: Buffer): void
+  _extractKeywords(text: string): string[]
+  _buildKeywordMap(keywords: string[]): Record<string, number>
+  _scoreDoc(bm25Doc: BM25Doc, keywords: string[]): number
+  _fuzzyMatchKeywords(keywords: string[]): string[]
+  _calculateIdf(): void
+  _calculateAvgLength(): void
+  _validateExtractor(extractor: (text: string) => string[]): void
+  _validateDocument(doc: Doc): void
+
+  extractor: (text: string) => string[]
+  extractorHash: string
+  version: number
+  fuzzyThreshold: number
+  k1: number
+  b: number
+  bm25Docs: Record<string, BM25Doc>
+  avgLength: number
+  idf: Record<string, number>
+  keywordSet: Set<string>
+}
+
+export declare function levenshteinSimilarity(s1: string, s2: string): number

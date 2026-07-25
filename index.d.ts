937967a60b5fbd12fc2589b1c234a42744472746
Connor Skelland <34070858+Connor56@users.noreply.github.com>
2026-07-25 22:04:54 +0100

    fix: add a root types file

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+export { BM25Index } from './lib/bm25'
+export type { BM25IndexOptions, BM25Doc } from './lib/bm25'
+
+export { VectorIndex } from './lib/vector'
+export type { VectorIndexOptions, HuggingFaceExtractorType, HuggingFaceTensor } from './lib/vector'
+
+export type { Doc, SearchResult } from './lib/models'
